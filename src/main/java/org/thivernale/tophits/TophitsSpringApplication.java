66af0ce43d60e472ce10f16ec5f24c8f782304a7
thivernale <thivernale@gmail.com>
2025-11-01 18:09:56 +0200

    Initial commit

---
+++ src/main/java/org/thivernale/tophits/TophitsSpringApplication.java
@@ -0,0 +1,12 @@
+package org.thivernale.tophits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TophitsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TophitsSpringApplication.class, args);
+    }
+}
---
+++ src/test/java/org/thivernale/tophits/TophitsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thivernale.tophits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TophitsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
