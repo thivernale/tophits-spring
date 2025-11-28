7629abfff9168fb54a6e091f0ff4c5014c68c897
thivernale <thivernale@gmail.com>
2025-11-28 14:32:44 +0200

    Updates

--- src/main/java/org/thivernale/tophits/config/AppConfig.java
+++ src/main/java/org/thivernale/tophits/config/AppConfig.java
@@ -1,7 +1,9 @@
 package org.thivernale.tophits.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +14,10 @@ import org.thivernale.tophits.filters.RateLimiter;
 import org.thivernale.tophits.filters.RateLimitingFilter;
 import org.thivernale.tophits.interceptors.LoggingInterceptor;
 import org.thivernale.tophits.interceptors.RateLimitingInterceptor;
+import org.thivernale.tophits.repositories.TrackRepository;
 import org.thivernale.tophits.services.AppListeners;
+
+import java.util.Arrays;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
@@ -48,5 +53,20 @@ public class AppConfig implements WebMvcConfigurer {
     @Bean
     ApplicationListener<ApplicationReadyEvent> publisher(ApplicationEventPublisher publisher) {
         return event -> publisher.publishEvent(new AppListeners.CustomAppEvent(event.getSource()));
+    }
+
+    //@Bean
+    public CommandLineRunner demo(ApplicationContext ctx, TrackRepository repo) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+            System.out.println(repo.count());
+        };
     }
 }
